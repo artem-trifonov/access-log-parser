0755be91e1c2f884f3b89704ca5520167aeeafda
Artem Trifonov <trifonov.artem.nn@gmail.com>
2025-12-07 16:27:06 +0300

    Task1: Обработка исключений

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
@@ -24,6 +26,34 @@ public class Main {
                 countCorretPath++;
             }
             System.out.println("Это файл номер " + countCorretPath + "\n");
+
+            try {
+                FileReader fileReader = new FileReader(path);
+                BufferedReader reader = new BufferedReader(fileReader);
+                String line;
+
+                int count = 0;
+                int maxL = 0;
+                int minL = Integer.MAX_VALUE;
+
+                while ((line = reader.readLine()) != null) {
+                    int length = line.length();
+                    count++;
+                    maxL = Math.max(maxL, length);
+                    minL = Math.min(minL, length);
+                    if (maxL > 1024)
+                        throw new LongLineException("Обнаружена строка длиной " + length + " Максимально допустимая длина - 1024 символа.");
+                }
+
+                System.out.println("Общее количество строк в файле: " + count);
+                System.out.println("Максимальная длина строки в файле: " + maxL);
+                System.out.println("Минимальная длина строки в файле: " + minL + "\n");
+            } catch (LongLineException ex) {
+                ex.printStackTrace();
+                return;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
         } while (true);
     }
 }
